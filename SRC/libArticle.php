e1919fff550430868a6df37c3e441f77939cdfa7
hiyamamasahiko <202406ap01_hiyama@x-ide.sakura.ne.jp>
2024-08-28 11:56:45 +0900

    #27577 & #27661
    新規登録したデータが検索結果に表示されない
    +　管理一覧画面で最初から除外にチェックが入っている
    記述漏れ、ミスをそれぞれ修正

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -19,7 +19,7 @@ function subArticle()
 	$orderTo = $_REQUEST['orderTo'];
 	$sPage   = $_REQUEST['sPage'];
 
-	if ($sDel = '') {
+	if ($sDel == '') {
 		$sDel = 1;
 	}
 
@@ -86,8 +86,8 @@ function subArticle()
 		return;
 	}
 	$sql = fnSqlArticleList(0, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo);
-	$res = mysql_query($sql);
-	$row = mysql_fetch_array($res);
+	$res = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_array($res);
 
 	$count = $row[0];
 
@@ -108,7 +108,7 @@ function subArticle()
 			</tr>
 			<?php
 			$sql = fnSqlArticleList(1, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo);
-
+			var_dump($sql);
 			$res = mysqli_query($conn, $sql);
 			$i = 0;
 			while ($row = mysqli_fetch_array($res)) {
